2422c5e40471507799fa688277443e0d3e9eafed
skochansky <stachu.kochanski@gmail.com>
2022-01-13 19:01:34 +0100

    #Post/delete Cars

--- API/models.py
+++ API/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Car(models.Model):
+    DoesNotExist = None
     make = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     avg_rating = models.DecimalField(max_digits=2, decimal_places=1, blank=True, null=True)
--- API/urls.py
+++ API/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CarView
+from .views import CarView, CarDeleteView
 
 urlpatterns = [
     path('cars', CarView.as_view()),
+    path('cars/<int:car_id>', CarDeleteView.as_view()),
 ]
