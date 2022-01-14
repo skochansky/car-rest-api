91afece9ab9d7e638e85f41c851c955514eff0c1
skochansky <stachu.kochanski@gmail.com>
2022-01-14 01:35:15 +0100

    #add API GET /popular request method.

--- API/views.py
+++ API/views.py
@@ -4,14 +4,17 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Car, CarRating
-from .service import check_if_car_exist
+from .service import check_if_car_exist, count_ratings, calculate_avg_rate_for_car
 import json
 import requests
+from collections import Counter
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CarView(View):
-    def get(self):
+    def get(self, request):
+        counter_list, ratings = count_ratings()
+        calculate_avg_rate_for_car(ratings, counter_list)
         cars = Car.objects.all()
         cars_serialized_data = []
         for car in cars:
@@ -20,11 +23,10 @@ class CarView(View):
                 'make': car.make,
                 'model': car.model,
                 'avg_rating': str(car.avg_rating)
-
             })
-        result = json.dumps(cars_serialized_data)
-        return HttpResponse(result, content_type='application/json', charset='UTF-8')
-
+            result = json.dumps(cars_serialized_data)
+        return HttpResponse(result, content_type='application/json',
+                            charset='UTF-8')
 
     def post(self, request):
         post_body = json.loads(request.body)
@@ -63,6 +65,30 @@ class CarRatingView(View):
         rate.save()
         result = {'message': 'Car rating added'}
         return HttpResponse(json.dumps(result), content_type='application/json',
+                            charset='UTF-8')
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CarPopularityView(View):
+    def get(self, request):
+        """Return top cars based on a number of rates"""
+        cars = Car.objects.all()
+        cars_serialized_data = []
+        ratings = count_ratings()
+        ratings = Counter(ratings[0])
+        for car in cars:
+            if car.id in ratings:
+                cars_serialized_data.append({
+                    'id': car.id,
+                    'make': car.make,
+                    'model': car.model,
+                    'number_of_rates': ratings[car.id]
+                })
+
+            cars_serialized_data = sorted(cars_serialized_data, key=lambda k: k['number_of_rates'], reverse=True)
+            result = json.dumps(cars_serialized_data)
+
+        return HttpResponse(result, content_type='application/json',
                             charset='UTF-8')
 
 
